08d27348e5abd54367d5db79a267ac6f1b6f4f57
ErdbeerenindenBergen <114823741+ErdbeerenindenBergen@users.noreply.github.com>
2023-02-24 14:47:11 -0500

    adding java file

---
+++ java/challenge-profisee/src/main/java/com/kassiburnett/challengeprofisee/ChallengeProfiseeApplication.java
@@ -0,0 +1,13 @@
+package com.kassiburnett.challengeprofisee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeProfiseeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeProfiseeApplication.class, args);
+	}
+
+}
---
+++ java/challenge-profisee/src/test/java/com/kassiburnett/challengeprofisee/ChallengeProfiseeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kassiburnett.challengeprofisee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeProfiseeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
